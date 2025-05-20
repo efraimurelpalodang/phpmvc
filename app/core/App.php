9e9141b0bbd7526b7fc9a6e4700a20735c8973b2
efraim urel <efraimurelpalodang@gmail.com>
2025-05-21 07:39:24 +0800

    selesai sampai set conntroller aja

--- app/core/App.php
+++ app/core/App.php
@@ -1,15 +1,31 @@
 <?php 
 
 class App {
+
+  protected $controller = 'home',
+            $method     = 'index',
+            $params     = [];
+  
   public function __construct()
   {
-    echo $this->parseURL();
+    $url = $this->parseURL();
+    
+    // pengecekan jika ada parameter yang dikirimakan
+    if( isset($_GET['url']) && file_exists("../app/controllers/$url[0].php") ) {
+      $this->controller = $url[0];
+      unset($url[0]);
+    }
+
+    var_dump($url);
+
   }
 
   public function parseURL()
   {
     if(isset($_GET['url'])) {
-      $url = $_GET['url'];
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
     }
     return $url;
   }
